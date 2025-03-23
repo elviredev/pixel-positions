680e33f14656b9b9c6e59640d02d1959e03d8596
elviredev <sandrine.rodriguez@hotmail.com>
2025-03-23 17:35:43 +0100

    ✨Fonctionnalité Rechercher un job par son titre et rechercher les jobs relatifs à un tag.

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use App\Models\Tag;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+   /**
+    * @desc Search jobs for this tag
+    * @route GET /tags/{tag:name}
+    * @param  Tag  $tag
+    * @return Factory|View|Application|\Illuminate\View\View|object
+    */
+   public function __invoke(Tag $tag)
+   {
+      return view('results', ['jobs' => $tag->jobs]);
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,19 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
+
+Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
+
+// Jobs
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 
 // Auth
 Route::middleware('guest')->group( function () {
